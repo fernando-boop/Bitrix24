a7badb072b3259a567400a536c74fb434feff4a3
Jacob <jacobmcad@gmail.com>
2020-04-14 23:43:52 -0400

    created US#7 AC#5

--- src/test/java/com/Bitrix24/pages/ActivityStream.java
+++ src/test/java/com/Bitrix24/pages/ActivityStream.java
@@ -14,5 +14,22 @@ public class ActivityStream {
     @FindBy(xpath = "//span[@class='feed-add-post-form-link feed-add-post-form-link-active']")
     public WebElement message;
 
+    @FindBy(xpath = "//span[@title='Upload files']")
+    public WebElement uploadFile;
+
+    @FindBy(xpath = "//span[@title='Quote text']")
+    public WebElement quoteIcon;
+
+    @FindBy(xpath = "//blockquote[@class='bxhtmled-quote']")
+    public WebElement blockquote;
+
+    @FindBy(xpath = "//button[@id='blog-submit-button-save']")
+    public WebElement sendButton;
+
+    @FindBy(xpath = "//span[@class='feed-add-post-form-link-text']")
+    public WebElement moreLink;
+
+    @FindBy(xpath = "//span[contains(text(), 'Announcement')][@class='menu-popup-item-text']")
+    public WebElement announcementLink;
 
 }
---
+++ src/test/java/com/Bitrix24/tests/US7.java
@@ -0,0 +1,52 @@
+package com.Bitrix24.tests;
+/*
+Author: Jacob
+7. As a user, I should be able to send annoucement from Annoucement subheadding from "More" under Activity Stream.
+    AC5. User should be able to create a quote by clicking on the Comma icon.
+ */
+import com.Bitrix24.base.TestBase;
+import com.Bitrix24.pages.ActivityStream;
+import com.Bitrix24.utilities.ConfigurationReader;
+import com.Bitrix24.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class US7 extends TestBase {
+
+    @Test
+    public void testAC5(){
+
+        //Login
+        loginPage.login(ConfigurationReader.getProperty("usernameHr9"), ConfigurationReader.getProperty("passwordHr9"));
+
+        //Navigate to More > Announcement
+        wait.until(ExpectedConditions.elementToBeClickable(activityStream.moreLink));
+        activityStream.moreLink.click();
+        wait.until(ExpectedConditions.elementToBeClickable(activityStream.announcementLink));
+        activityStream.announcementLink.click();
+
+        //Click quote icon to add quote
+        wait.until(ExpectedConditions.elementToBeClickable(activityStream.quoteIcon));
+        activityStream.quoteIcon.click();
+
+        //Type your quote
+        driver.switchTo().frame(driver.findElement(By.className("bx-editor-iframe")));
+        activityStream.blockquote.sendKeys("Hello World!");
+        driver.switchTo().defaultContent();
+
+        //Send your quote
+        activityStream.sendButton.click();
+
+    }
+
+    @AfterMethod
+    public void quit() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+}
